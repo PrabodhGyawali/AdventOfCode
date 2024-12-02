b74ba4d404de8b388aef8d95b3980c0c94b74316
PrabodhGyawali <prabodh.gyawali2005@gmail.com>
2024-12-02 00:16:35 +0000

    lol

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,17 +1,43 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
-// TODO: Binary Sort Algorithm in Rust
-// TODO: Request to get data and parse through it
-
-fn extract_puzzle_input() {
-	
+/* source: https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html */
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> 
+where P: AsRef<Path>, {
+	let file = File::open(filename)?;
+	Ok(io::BufReader::new(file).lines())
 }
 
-fn similarity_score(a: &Vec<i32>, b: &Vec<i32>) -> i32 {
+fn extract_puzzle_input(input_path: &str, a: &mut Vec<i32>, b: &mut Vec<i32>) {
+	if let Ok(lines) = read_lines(input_path) {
+		for line in lines.flatten() {
+			let tokens : Vec<&str> = line.split("   ").collect();
+			a.push(tokens[0].parse::<i32>().unwrap());
+			b.push(tokens[1].parse::<i32>().unwrap());
+		}
+	}
+}
+
+fn sort_vec(arr: &mut Vec<i32>) {
+	for i in 1..arr.len() {
+		let elem = arr[i];
+		let mut j = i as isize - 1;
+	
+		while j >= 0 && arr[j as usize] > elem {
+            arr[(j + 1) as usize] = arr[j as usize];
+            j -= 1;
+        }
+		arr[(j + 1) as usize] = elem;
+	}
+} // usize nonsense is dumb in Rust
+
+fn similarity_score(a: &mut Vec<i32>, b: &mut Vec<i32>) -> i32 {
 	let mut score: i32 = 0;
 	
-	// Sort list
+	sort_vec(a);
+	sort_vec(b);
 
-	// Each list index find difference
 	for n in 0..a.len() {
 		score += (a[n] - b[n]).abs();
 	}
@@ -20,8 +46,7 @@ fn similarity_score(a: &Vec<i32>, b: &Vec<i32>) -> i32 {
 }
 
 fn main() {
-    let a = vec![1, 2, 3];
-	let b = vec![2, 2, 10];
-	let result = similarity_score(&a, &b);
-	println!("result: {:?}", result);
+	let (mut a, mut b): (Vec<i32>, Vec<i32>) = (Vec::new(), Vec::new());
+	extract_puzzle_input("./input.txt", &mut a, &mut b);
+	println!("score: {}", similarity_score(&mut a, &mut b));
 }
