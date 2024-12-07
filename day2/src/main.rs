35ef1428ce7cf9c4474fa24854de28d3ac6b8625
PrabodhGyawali <prabodh.gyawali2005@gmail.com>
2024-12-07 21:17:23 +0000

    day 2 complete

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -22,35 +22,38 @@ fn get_dir(a: &i32, b: &i32) -> i32{
 
 fn get_safe_report(reports: &mut Vec<Vec<i32>>) -> usize {
     let count = 
-        reports.iter()
-        .filter(|levels|{
-            let mut safe = true;
-            let dir = get_dir(&levels[0], &levels[1]);
-
-            for i in 0..levels.len()-1 {
-                let diff = (levels[i] - levels[i+1]).abs();
-                let cur_dir = get_dir(&levels[i], &levels[i+1]);
-
-                print!("{}: {} {} || diff: {}; ", i, dir, cur_dir, diff);
-                if dir != cur_dir {
-                    println!("reached dir");
-                    safe = false;
-                    break;
-                }
-                
-                if !(diff == 1 || diff == 2 || diff == 3) {
-                    println!("reached diff");
-                    safe = false;
-                    break;
-                }
+        reports.iter().filter(|report|{
+            let mut damped_reports = Vec::new();
+            for i in 0..report.len() {
+                let mut damped_report = (*report).clone();
+                damped_report.remove(i);
+                println!("{:?}", damped_report);
+                damped_reports.push(damped_report);
             }
-            println!(" ");
+            damped_reports.iter().any(|levels|{
+                let mut safe = true;
+                let dir = get_dir(&levels[0], &levels[1]);
 
-            
-            safe       
+                for i in 0..levels.len()-1 {
+                    let diff = (levels[i] - levels[i+1]).abs();
+                    let cur_dir = get_dir(&levels[i], &levels[i+1]);
+
+                    if dir != cur_dir {
+                        safe = false;
+                        break;
+                    }
+                    
+                    if !(diff == 1 || diff == 2 || diff == 3) {
+                        safe = false;
+                        break;
+                    }
+                }
+
+                safe
+            })   
         }).count();
     count
-}
+} /* Need to improve function and Kogmolorov Complexity */
 
 fn main() {
     let mut reports: Vec<Vec<i32>> = parse_input();
